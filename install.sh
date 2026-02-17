ad5ffbc0c8d7586dbbace69da6dcaae24c9ae0ca
Joangel Reyes <joangelr28@protonmail.com>
2026-02-17 14:34:29 -0400

    Moves dotfiles to the home directory
    Refactors the dotfiles structure to adhere to the XDG base directory specification.
    This change ensures that application configurations are stored in their proper locations within the home directory (e.g., ~/.config).
    It also introduces an installation script that automates the process of setting up the dotfiles, including creating base folders and managing SSH keys.

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# TODO: Install required packages with homebrew
+# brew install -y git stow
+
+# Welcome
+echo "Welcome to the Instalation Script"
+
+if [[ ! -d "$HOME/dotFiles" ]]; then
+  echo "$HOME/dotFiles folder is required, cloning it"
+  cd "$HOME" || exit
+  git clone https://github.com/OsiNubis99/dotFiles.git ~/dotFiles
+fi
+
+echo "Creating base Folders"
+mkdir -p ~/.config
+mkdir -p ~/.local/share
+
+## Backup
+if [[ -d "$HOME/backup" ]]; then
+  echo "Stow backup folder"
+  stow -d "$HOME/backup/" -t "$HOME/" .
+  if [[ -e "$HOME/.ssh/personal_key" ]]; then
+    chmod -R 700 ~/.ssh
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/personal_key
+    echo "Personal ssh key added"
+  fi
+fi
+
+echo "Stow home folder"
+stow -d "$HOME/dotFiles/home/" -t "$HOME/" . --adopt
+# Final instalation
+echo "Instalation Complete!"
